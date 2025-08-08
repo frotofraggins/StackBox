5a46306d1752fdb5b47915ad9fd8e5019429bd9d
Nick Flournoy <nflos@amazon.com>
2025-08-08 15:54:34 -0700

    Remove problematic @fontsource/inter import to fix build error

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import '@aws-amplify/ui-react/styles.css'
-import '@fontsource/inter'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
